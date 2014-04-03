23ef425560683753241374d0825a264f7951dfbc
tsalmon <th_s@hotmail.fr>
2014-04-03 23:50:23 +0200

    new parameters for Figure (border & color)

--- src/Figure.java
+++ src/Figure.java
@@ -1,11 +1,15 @@
+import java.awt.Color;
 import java.awt.Polygon;
-
 
 public class Figure extends Polygon{
 	int id_fig;
 	int xPol[];
 	int yPol[];
 	int nPol;
+	
+	Color border_color;
+	int   border_width;
+	Color fil_color;
 	
 	Figure(int id, int[] xP, int[] yP, int size){
 		super(xP, yP, size);
