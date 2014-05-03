871bfaf49823389a93e71081c600587ec8f7c6ab
tsalmon <th_s@hotmail.fr>
2014-05-03 09:11:56 +0200

    View extended from JPanel, not from Canvas

--- src/Vue.java
+++ src/Vue.java
@@ -2,30 +2,30 @@ import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
-public class Vue extends Canvas{
+import javax.swing.JPanel;
+
+public class Vue extends JPanel{
 		int centerX, centerY;
 		float rWidth = 16.0F, rHeight = 9F, pixelSize;
 		float xP = 1000000, yP;
 		int left, right, top, bottom;
 		void initgr() {
-			Dimension d = getSize();
+			Dimension d = this.getSize();
 			int maxX = d.width - 1, maxY = d.height - 1;
 			pixelSize = Math.max(rWidth/maxX, rHeight/maxY);
 			centerX = maxX/2; centerY = maxY/2;
 		}
 
 		  public void paint(Graphics g){
-			    initgr();
-			    
-			    left = iX(-rWidth/2);
-			    right = iX(rWidth/2);
-				bottom = iY(-rHeight/2);
-				top = iY(rHeight/2);
-				
-				g.drawRect(left, top, right - left, bottom - top);
-			    if(xP != 1000000)
-			      g.drawString("Logical coordinates of selected point: "
-			        + xP + ", " + yP, 20, 100);
+			  initgr();
+			  left = iX(-rWidth/2);
+			  right = iX(rWidth/2);
+			  bottom = iY(-rHeight/2);
+			  top 	= iY(rHeight/2);
+			  g.drawRect(left, top, right - left, bottom - top);
+			  if(xP != 1000000)
+				  g.drawString("Logical coordinates of selected point: "
+						 + xP + ", " + yP, 20, 100);
 			  }
 
 		
