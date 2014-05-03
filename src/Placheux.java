e7fc319c51e2b5ee2da4e24eddbbe3f4bf55edd1
tsalmon <th_s@hotmail.fr>
2014-05-04 01:29:31 +0200

    the View is a PanElem (extend JPane)

--- src/Placheux.java
+++ src/Placheux.java
@@ -15,6 +15,8 @@ import java.util.*;
 public class Placheux extends JPanel{
 
 	JPopupMenu menu = new JPopupMenu();
+	boolean menu_launched = false;
+	Shape fig_inc = new GeneralPath(); // the figure we do
 
 	JButton lecture_pause = new JButton("lecture");
 	JButton stop = new JButton("stop");	
@@ -22,11 +24,17 @@ public class Placheux extends JPanel{
 
 	JTable tab;
 
-	JPanel view;
+	PanElem view;
 
+	/*
+	 * (origin_x, origin_y) is the point of beginning paint
+	 */
+	int origin_x, origin_y;
+	
 	//listener of popupmenu
 	ActionListener aListener = new ActionListener() {
 		public void actionPerformed(ActionEvent event) {
+			choix_menu(event.getActionCommand());
 		}
 	};
 
@@ -38,8 +46,9 @@ public class Placheux extends JPanel{
 
 	Placheux(){
 		Controller controller = new Controller();
+		JPopupMenu.setDefaultLightWeightPopupEnabled(false);
 
-		view = new JPanel();
+		view = new PanElem();
 		view.setBorder(BorderFactory.createLineBorder(Color.red));
 		view.setPreferredSize(new Dimension(1200, 900));
 		final JScrollPane panel_view = new JScrollPane(view);
@@ -82,16 +91,208 @@ public class Placheux extends JPanel{
 		panel_view.addMouseMotionListener(controller);
 	}
 
-	public void init_popup_menu()
+	public void init_menu_createFigure()
 	{
 		System.out.println("init_popup_menu");
 		menu.addPopupMenuListener(pListener);
 
-		JMenuItem editItem = new JMenuItem("Rectangle");
-		editItem.addActionListener(aListener);
-		menu.add(editItem);		
-	}
+		JMenuItem rectItem = new JMenuItem("Rectangle");
+		rectItem.addActionListener(aListener);
+		menu.add(rectItem);	
 
+		JMenuItem sqrtItem = new JMenuItem("Carré");
+		sqrtItem.addActionListener(aListener);
+		menu.add(sqrtItem);	
+
+		JMenuItem circleItem = new JMenuItem("Cercle");
+		circleItem.addActionListener(aListener);
+		menu.add(circleItem);	
+
+	}
+	
+	public void choix_menu(String choix){
+		fig_inc = view.draw_rect();
+	}
+	
+	class PanElem extends JPanel{
+		final Color couleurBord = Color.red;
+		final Color couleurInterieur = Color.blue;
+		final Color couleurFond = Color.black;
+		Shape figure = null;
+		LinkedList<Integer> diy = new LinkedList<Integer>();
+		int a, b;
+		int x, y;
+
+		PanElem() { 
+			setPreferredSize(new Dimension(600, 400));
+		}
+
+		/*It's might be usefull for drawing stars
+		private double points[][] = { 
+				{ 0, 85 }, { 75, 75 }, { 100, 10 }, { 125, 75 }, 
+				{ 200, 85 }, { 150, 125 }, { 160, 190 }, { 100, 150 }, 
+				{ 40, 190 }, { 50, 125 }, { 0, 85 } 
+		};*/
+
+		private void doDrawing(Graphics g) {
+			if(id_fig == 0){
+				GeneralPath gp = new GeneralPath();
+				gp.moveTo(a, b);
+				gp.lineTo(x, y);
+				gp.closePath();
+				fig_inc = gp;
+			} else if (id_fig == 1){
+				fig_inc = draw_circle();
+			} else if(id_fig == 2){
+				fig_inc = draw_rect();
+			}  else if(id_fig == 3){				
+				fig_inc = draw_cross();
+			} else if(id_fig == 4){	
+				fig_inc = draw_iso();
+			} else if(id_fig == 5){
+				fig_inc = draw_equi();
+			} else if(id_fig == 6){
+				fig_inc = draw_arrow();
+			} else if(id_fig == 7){
+				fig_inc = this.draw_star();
+			} else if(id_fig == 8){
+				GeneralPath aux = new GeneralPath();
+				aux.moveTo(diy.get(0), diy.get(1));
+				for(int i = 0; i < diy.size(); i+=2){
+					aux.lineTo(diy.get(i), diy.get(i+1));
+				}
+				aux.closePath();
+				fig_inc = aux;
+			} else {
+				System.out.println("start: quit");
+				return ;
+			}
+		}
+
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			if(fig_inc == null){
+				doDrawing(g);
+			} else {
+				
+				Graphics2D g2d = (Graphics2D)g;
+
+				g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+						RenderingHints.VALUE_ANTIALIAS_ON);
+
+				g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
+						RenderingHints.VALUE_RENDER_QUALITY);
+
+				g2d.setColor(Color.BLACK); /** TODO: fill color **/
+				doDrawing(g);
+
+				g2d.fill(fig_inc);  
+
+				g2d.setColor(Color.blue); /** TODO: border color & size **/
+				g2d.setStroke(new BasicStroke(3));
+				g2d.draw(fig_inc);
+
+				g2d.dispose();
+				
+			}
+		}
+
+		public void init_a_b(int a, int b){
+			this.a = a;
+			this.b = b;
+		}
+
+		public Shape draw_circle(){
+			int u = (x < a) ? x : a ,
+					v = (y < b) ? y : b;
+			int k = (x < a) ? a-x : x-a ,
+					l = (y < b) ? b-y : y-b;
+			
+			Ellipse2D p = new Ellipse2D.Double(u,v, k, l);
+			return p;
+		}
+
+		public Shape draw_cross(){
+			GeneralPath p = new GeneralPath();
+			p.moveTo((3*x + a)/4, b);
+			p.lineTo((3*a + x)/4, b);
+			p.lineTo((3*a + x)/4, (3*b + y)/4);
+			p.lineTo(a			, (3*b + y)/4);
+			p.lineTo(a			, (3*y + b)/4);
+			p.lineTo((3*a + x)/4, (3*y + b)/4);
+			p.lineTo((3*a + x)/4, y);
+			p.lineTo((3*x + a)/4, y); 
+			p.lineTo((3*x + a)/4, (3*y + b)/4);
+			p.lineTo(x			, (3*y + b)/4);
+			p.lineTo(x			, (3*b + y)/4);
+			p.lineTo((3*x + a)/4, (3*b + y)/4); 
+			p.closePath();
+			return p;
+		}
+
+		public Shape draw_rect(){
+			GeneralPath p = new GeneralPath();
+			p.moveTo(x, b);
+			p.lineTo(a, b);
+			p.lineTo(a, y);
+			p.lineTo(x, y);
+			p.closePath();
+			return p;
+		}
+
+		public Shape draw_iso(){	
+			GeneralPath p = new GeneralPath();
+
+			p.moveTo((x+a)/2, b);
+			p.lineTo(a, y);
+			p.lineTo(x, y);
+			return p;
+		}
+
+		public Shape draw_star(){
+			GeneralPath p = new GeneralPath();
+			double radius = Math.sqrt(Math.pow(a-x, 2) + Math.pow(b-y, 2));
+			double angle = Math.PI * 2 / 10;
+
+			for (int i = 0; i < 10; i++) {
+				double an = angle * i;
+
+				double x = a + ((Math.cos(an) * (radius + radius * (1 * (i%2)))));
+				double y = b + ((Math.sin(an) * (radius + radius * (1 * (i%2)))));
+				if (i == 0) {
+					p.moveTo(x, y);
+				} else {	
+					p.lineTo(x, y);
+				}
+			}
+			p.closePath();
+			return p;
+		}
+
+		public Shape draw_equi(){
+			GeneralPath p = new GeneralPath();
+			double angle = Math.toRadians(60);
+
+			p.moveTo(a, b);
+			p.lineTo(x, y);
+			p.lineTo((int)((Math.cos(angle) * ((a-x)) - Math.sin(angle) * ((b-y))) + x), (int)((Math.sin(angle) * ((a-x)) + Math.cos(angle) * ((b-y))) + y));
+			return p;
+		}
+
+		public Shape draw_arrow(){			
+			GeneralPath p = new GeneralPath();
+			p.moveTo(a		, (b+y)/2);
+			p.lineTo((a+x)/2, y);
+			p.lineTo((a+x)/2, (b+3*y)/4);
+			p.lineTo(x		, (b + 3*y)/4);//4
+			p.lineTo(x		, (y + 3*b)/4);
+			p.lineTo((a+x)/2, (y + 3*b)/4);
+			p.lineTo((a+x)/2, b);
+			p.closePath();
+			return p;
+		}
+	}
+	
 	class Controller 
 	extends MouseInputAdapter 
 	implements ActionListener, ComponentListener{
@@ -104,7 +305,7 @@ public class Placheux extends JPanel{
 		public Figure getFigureSelected(int x, int y){
 			return null;
 		}
-		
+
 		public boolean clickG(MouseEvent e){
 			return (SwingUtilities.isLeftMouseButton(e));
 		}
@@ -112,7 +313,7 @@ public class Placheux extends JPanel{
 		public boolean clickD(MouseEvent e){
 			return (SwingUtilities.isRightMouseButton(e));
 		}
-		
+
 		public void actionPerformed(ActionEvent e) {
 			System.out.print ("ActionPerformed: ");
 		}
@@ -129,6 +330,13 @@ public class Placheux extends JPanel{
 		public void mouseReleased(MouseEvent e) {
 			System.out.print("mouseReleased: ");
 			System.out.println(getSize());
+			if(menu_launched) // If right after right click: exit menu
+			{
+				menu.removeAll();
+				menu_launched = false;
+				repaint();
+				return;
+			}
 			if(e.getSource() == tab){
 				System.out.println("tab");			
 				int tabx = tab.getSelectedColumn();
@@ -141,12 +349,15 @@ public class Placheux extends JPanel{
 				Figure f = getFigureSelected(e.getX(), e.getY());
 				if(f == null){ // click on void screen
 					System.out.println("vide");
+					 init_menu_createFigure();
 				} else {
 					System.out.println("une figure");
 				}
+				menu.show(e.getComponent(), e.getX(), e.getY());
+				menu_launched = true;
 			}
 		}
-		
+
 		public void mouseDragged (MouseEvent e) {
 			System.out.print("mouseDragged: ");
 			if(e.getSource() == view){
