7cfcc53e21c14bc2da50514f316f924fc2d1c37e
tsalmon <th_s@hotmail.fr>
2014-06-02 00:58:37 +0200

    transition

--- src/PanneauNouvelleAnimation.java
+++ src/PanneauNouvelleAnimation.java
@@ -19,10 +19,14 @@ import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 
+import model.animation.EasingType;
+import model.animation.Translation;
 import model.easing.Back;
 import model.easing.Bounce;
 import model.easing.Circ;
 import model.easing.Cubic;
+import model.easing.Easing;
+import model.easing.Easing.EASING;
 import model.easing.Elastic;
 import model.easing.Expo;
 import model.easing.Linear;
@@ -30,18 +34,22 @@ import model.easing.Quad;
 import model.easing.Quart;
 import model.easing.Quint;
 import model.gestionnary.StateGestionnary;
+import model.movable.Figure;
+import model.movable.Movable;
+import model.movable.PointPlacheux;
 
 
 public class PanneauNouvelleAnimation extends JDialog implements ActionListener{
 	private static final long serialVersionUID = 1L;
 
 	String[] easings = new String[]{"Linear", "Back", "Bounce", "Circ", "Cubic", "Elastic", "Expo", "Quad", "Quart", "Quint", "Sine"};
-	String[] easingTypes = new String[]{"None", "Ease In", "Ease Out", "Ease In-Out"};
+	String[] easingTypes = new String[]{"easeNone", "easeIn", "easeOut", "easeInOut"};
 
 	JSpinner debutAnimation = null;
 	JSpinner finAnimation = null;
-	
+
 	private JComboBox<String> easing_select = new JComboBox<String>();
+	private JComboBox<String> easing_type_select = new JComboBox<String>();
 	private JComboBox<String> anim_select = new JComboBox<String>();
 	JPanel panel_animation = new JPanel();
 	JFrame ecran;
@@ -52,14 +60,17 @@ public class PanneauNouvelleAnimation extends JDialog implements ActionListener{
 
 	int figure_x, figure_y;
 	StateGestionnary data;
+	Figure figure;
 
 	JTextField[] champs;
-	
-	public PanneauNouvelleAnimation(JFrame ecran, StateGestionnary data, int currentTime, int finFilm, int x_fig, int y_fig) {
+	JTextField nom_animation = new JTextField(5);
+	int[] champs_valeurs;
+
+	public PanneauNouvelleAnimation(JFrame ecran, Figure figure, StateGestionnary data, int currentTime, int finFilm, int x_fig, int y_fig) {
 		this.setModal(true);
 		this.setLocation(400, 200);
 		this.setTitle("Nouvelle animation");
-		this.setSize(800, 600);		
+		this.setSize(800, 600);
 		this.setLayout(new BorderLayout());
 		this.data = data;
 		this.debutAnimation = new JSpinner(new SpinnerNumberModel(currentTime,0,finFilm,1));
@@ -69,11 +80,12 @@ public class PanneauNouvelleAnimation extends JDialog implements ActionListener{
 		this.setLayout(boxLayout);
 		this.figure_x = x_fig;
 		this.figure_y = y_fig;
-		
+		this.figure = figure;
+
 		init_easingSelect();
 		init_animeSelect();
 
-		panel_temps();
+		panel_general();
 		init_panel_anime(0);		
 		this.add("Center", this.panel_animation);
 		panel_btn();
@@ -89,11 +101,13 @@ public class PanneauNouvelleAnimation extends JDialog implements ActionListener{
 
 	public void init_easingSelect(){
 		for(int i = 0; i < easings.length; i++){
-			for(int j = 0; j < easingTypes.length; j++){
-				easing_select.addItem(easings[i] + " - " + easingTypes[j]);				
-			}
-		}		
+			easing_select.addItem(easings[i]);
+		}
+		for(int j = 0; j < easingTypes.length; j++){
+			easing_type_select.addItem(easingTypes[j]);
+		}
 		easing_select.setSelectedIndex(0);
+		easing_type_select.setSelectedIndex(0);
 	}
 
 	public void init_animeSelect(){
@@ -111,20 +125,24 @@ public class PanneauNouvelleAnimation extends JDialog implements ActionListener{
 			champs[i] = new JTextField(5);
 		}
 	}
-	
-	public void panel_temps(){
+
+	public void panel_general(){
 		JPanel panel_time = new JPanel();
-		panel_time.setLayout(new GridLayout(2, 2));
-		
-		panel_time.add(easing_select);
-		
+		panel_time.setLayout(new GridLayout(3, 2));
+
+		JPanel panel_nom = new JPanel();
+		panel_nom.add(new JLabel("Nom"));
+		panel_nom.add(nom_animation);
+		panel_time.add(panel_nom);
 		panel_time.add(anim_select);
-		
+		panel_time.add(easing_select);
+		panel_time.add(easing_type_select);
+
 		JPanel panel_debutAnime = new JPanel();
 		panel_debutAnime.add(new JLabel("debut animation"));
 		panel_debutAnime.add(this.debutAnimation);
 		panel_time.add(panel_debutAnime);
-		
+
 		JPanel panel_finAnime = new JPanel();
 		panel_finAnime.add(new JLabel("fin animation"));
 		panel_finAnime.add(this.finAnimation);
@@ -135,7 +153,7 @@ public class PanneauNouvelleAnimation extends JDialog implements ActionListener{
 	public void panel_translation(){
 		init_textfield(4);
 		this.panel_animation.setLayout(new GridLayout(2, 2));
-		
+
 		JPanel trans_x = new JPanel();
 		trans_x.add(new JLabel("position initiale x"));
 		trans_x.add(champs[0]);
@@ -160,7 +178,7 @@ public class PanneauNouvelleAnimation extends JDialog implements ActionListener{
 	public void panel_rotation(){
 		init_textfield(3);
 		this.panel_animation.setLayout(new BoxLayout(this.panel_animation, BoxLayout.Y_AXIS));
-		
+
 		JPanel rot_x = new JPanel();
 		rot_x.add(new JLabel("position initiale x"));
 		rot_x.add(champs[0]);
@@ -180,7 +198,7 @@ public class PanneauNouvelleAnimation extends JDialog implements ActionListener{
 
 	public void panel_echelle(){
 		init_textfield(1);
-		
+
 		JPanel panel_size = new JPanel();
 		panel_size.add(new JLabel("Taille de fin"));
 		panel_size.add(champs[0]);
@@ -189,11 +207,11 @@ public class PanneauNouvelleAnimation extends JDialog implements ActionListener{
 	}
 
 	public void panel_background(){
-		
+
 		JPanel panel_size = new JPanel();
 		btn_color.setText("Couleur de fond");
 		panel_size.add(btn_color);
-		
+
 		this.panel_animation.add(panel_size);		
 	}
 
@@ -201,12 +219,12 @@ public class PanneauNouvelleAnimation extends JDialog implements ActionListener{
 		init_textfield(1);
 		this.panel_animation.setLayout(new BoxLayout(this.panel_animation, BoxLayout.Y_AXIS));
 		btn_color.setText("Couleur de bordure");
-		
+
 		JPanel panel_size = new JPanel();
 		panel_size.add(new JLabel("Taille de fin"));
 		panel_size.add(champs[0]);
 		this.panel_animation.add(panel_size);
-		
+
 		this.panel_animation.add(btn_color);
 	}
 
@@ -216,7 +234,7 @@ public class PanneauNouvelleAnimation extends JDialog implements ActionListener{
 		case 1: panel_rotation();break;
 		case 2: panel_echelle();break;
 		case 3: panel_background();break;
-		case 4: panel_bordure();break;
+		case 4: panel_bordure();
 		}
 	}
 
@@ -226,6 +244,104 @@ public class PanneauNouvelleAnimation extends JDialog implements ActionListener{
 		panel_bouton.add(this.ok_btn);	
 		panel_bouton.add(this.cancel_btn);
 		this.add("South", panel_bouton);	
+	}
+
+	public void ajoutAnimation(int id_animation){
+		switch(id_animation){
+		case 0: ajoutTranslation(); break;
+		case 1: ajoutRotation(); break;
+		case 2: ajoutEchelle(); break;
+		case 3: ajoutBackground(); break;
+		case 4: ajoutBordure();
+		}
+	}
+
+	private void ajoutBordure() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void ajoutBackground() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void ajoutEchelle() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void ajoutRotation() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void ajoutTranslation() {
+		Translation t = new Translation(this.nom_animation.getText(),
+				this.figure,
+				(double)this.debutAnimation.getValue(),
+				(double)this.finAnimation.getValue(),
+				fonctionEasing(this.easing_select.getSelectedIndex()),
+				fonctionEasingType(this.easing_type_select.getSelectedIndex()),						
+				new PointPlacheux(this.champs_valeurs[0], this.champs_valeurs[1]),
+				new PointPlacheux(this.champs_valeurs[2], this.champs_valeurs[4]));
+	}
+
+	private EasingType fonctionEasingType(int selectedIndex) {
+		return EasingType.getType(this.easing_type_select.getSelectedItem().toString());
+	}
+
+	private Easing fonctionEasing(int selectedIndex) {
+		try{
+			switch(selectedIndex){
+			case 0: return Easing.getEasing(Easing.EASING.LINEAR);
+			case 1: return Easing.getEasing(Easing.EASING.BACK);
+			case 2: return Easing.getEasing(Easing.EASING.BOUNCE);
+			case 3: return Easing.getEasing(Easing.EASING.CIRC);
+			case 4: return Easing.getEasing(Easing.EASING.CUBIC);
+			case 5: return Easing.getEasing(Easing.EASING.ELASTIC);
+			case 6: return Easing.getEasing(Easing.EASING.EXPO);
+			case 7: return Easing.getEasing(Easing.EASING.QUAD);
+			case 8: return Easing.getEasing(Easing.EASING.QUART);
+			case 9: return Easing.getEasing(Easing.EASING.QUINT);
+			case 10: return Easing.getEasing(Easing.EASING.SINE);
+			}
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private boolean convertTextFieldToText(){
+		this.champs_valeurs = new int[this.champs.length];
+		try{
+			for(int i = 0; i < champs_valeurs.length; i++){
+				champs_valeurs[i] = Integer.parseInt(this.champs[i].getText());
+			}
+		} catch(NumberFormatException e){
+			JOptionPane.showMessageDialog(this, "les champs doivent etre des nombres");
+			return false;
+		}
+		return true;
+	}
+
+	public boolean verifieNomAnimation(){
+		if(this.nom_animation.getText().length() == 0){
+			JOptionPane.showMessageDialog(this, "Vous devez donner un nom à l'animation.");
+			return false;
+		}
+		if(!this.nom_animation.getText().matches("^[a-zA-Z0-9 ]*$")){
+			JOptionPane.showMessageDialog(this, "Vous devez donner un nom alphanumerique (et qui commence par une lettre");
+			return false;
+		}
+		char c = '0';
+		for(int i = 0; i < 10; i++){
+			if(this.nom_animation.getText().charAt(0) == c){
+				JOptionPane.showMessageDialog(this, "Vous devez donner un nom alphanumerique (et qui commence par une lettre");
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -241,6 +357,12 @@ public class PanneauNouvelleAnimation extends JDialog implements ActionListener{
 						"La durée de l'animation est incorrecte");
 				return;
 			}
+
+			if(!this.verifieNomAnimation()) return;
+
+			if(!this.convertTextFieldToText()) return;
+
+			ajoutAnimation(this.anim_select.getSelectedIndex());
 			this.setVisible(false);
 		}
 		if(e.getSource() == this.cancel_btn){
