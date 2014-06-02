b8fef7ed096ad0405f48cf042f243522e66907a5
Josian Chevalier <josian.chevalier@gmail.com>
2014-06-02 03:27:02 +0200

    while infiniy brisé

--- src/model/gestionnary/StateGestionnary.java
+++ src/model/gestionnary/StateGestionnary.java
@@ -353,6 +353,7 @@ public class StateGestionnary {
                 Graphics2D graphics = image.createGraphics();
                 graphics.setPaint (film.getBackgroundColor());
                 graphics.fillRect ( 0, 0, image.getWidth(), image.getHeight() );
+                System.out.println(t+"      "+film.getDuration());
                 for(ListIterator<Figure> li = figures.listIterator(); li.hasNext();){                    
                     Figure next = li.next();
                     graphics.setColor(next.getColor());
@@ -360,8 +361,10 @@ public class StateGestionnary {
                     graphics.setColor(next.getBorderColor());
                     graphics.setStroke(new BasicStroke(Math.round(next.getStrokeThickness())));
                     graphics.draw(next.getShape());
+                    System.out.println(figures);
                 }                                
                 result.add(image);
+                t+=1000/framerate;
             }
             return result;
         }
